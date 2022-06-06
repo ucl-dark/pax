e768a01efbf644f9b841ed5c744675c386adc0c0
Newton Kwan <nk@2398.com>
2022-06-06 19:10:57 +0100

    add tests for more normal games

--- test/test_env.py
+++ test/test_env.py
@@ -5,14 +5,26 @@ import pytest
 from pax.env import SocialDilemmaBaseEnvironment
 from pax.strategies import TitForTat
 
+# payoff matrices for four games
+ipd = [[2, 2], [3, 0], [0, 3], [1, 1]]
+stag = [[4, 4], [3, 1], [1, 3], [2, 2]]
+sexes = [[3, 2], [0, 0], [0, 0], [2, 3]]
+chicken = [[0, 0], [1, -1], [-1, 1], [-2, -2]]
+test_payoffs = [ipd, stag, sexes, chicken]
 
-def test_single_batch_rewards() -> None:
+
+@pytest.mark.parametrize("payoff", test_payoffs)
+def test_single_batch_rewards(payoff) -> None:
     num_envs = 1
-    payoff = [[2, 2], [3, 0], [0, 3], [1, 1]]
-    # payoff = [[4,4], [3,1], [1,3], [2,2]]
     env = SocialDilemmaBaseEnvironment(5, num_envs, payoff)
     action = jnp.ones((num_envs, 1), dtype=jnp.int32)
     r_array = jnp.ones((num_envs, 1), dtype=jnp.int32)
+
+    # payoffs
+    cc_p1, cc_p2 = payoff[0][0], payoff[0][1]
+    dc_p1, dc_p2 = payoff[1][0], payoff[1][1]
+    cd_p1, cd_p2 = payoff[2][0], payoff[2][1]
+    dd_p1, dd_p2 = payoff[3][0], payoff[3][1]
 
     # first step
     tstep_0, tstep_1 = env.step((0 * action, 0 * action))
@@ -20,36 +32,46 @@ def test_single_batch_rewards() -> None:
     assert tstep_1.reward is None
 
     tstep_0, tstep_1 = env.step((0 * action, 0 * action))
-    assert jnp.array_equal(tstep_0.reward, 2 * r_array)
-    assert jnp.array_equal(tstep_1.reward, 2 * r_array)
+    assert jnp.array_equal(tstep_0.reward, cc_p1 * r_array)
+    assert jnp.array_equal(tstep_1.reward, cc_p2 * r_array)
 
     tstep_0, tstep_1 = env.step((1 * action, 0 * action))
-    assert jnp.array_equal(tstep_0.reward, 3 * r_array)
-    assert jnp.array_equal(tstep_1.reward, 0 * r_array)
+    assert jnp.array_equal(tstep_0.reward, dc_p1 * r_array)
+    assert jnp.array_equal(tstep_1.reward, dc_p2 * r_array)
 
     tstep_0, tstep_1 = env.step((0 * action, 1 * action))
-    assert jnp.array_equal(tstep_0.reward, 0 * r_array)
-    assert jnp.array_equal(tstep_1.reward, 3 * r_array)
+    assert jnp.array_equal(tstep_0.reward, cd_p1 * r_array)
+    assert jnp.array_equal(tstep_1.reward, cd_p2 * r_array)
 
     tstep_0, tstep_1 = env.step((1 * action, 1 * action))
-    assert jnp.array_equal(tstep_0.reward, 1 * r_array)
-    assert jnp.array_equal(tstep_1.reward, 1 * r_array)
+    assert jnp.array_equal(tstep_0.reward, dd_p1 * r_array)
+    assert jnp.array_equal(tstep_1.reward, dd_p2 * r_array)
 
 
 testdata = [
-    ((0, 0), (2, 2)),
-    ((1, 0), (3, 0)),
-    ((0, 1), (0, 3)),
-    ((1, 1), (1, 1)),
+    ((0, 0), (2, 2), ipd),
+    ((1, 0), (3, 0), ipd),
+    ((0, 1), (0, 3), ipd),
+    ((1, 1), (1, 1), ipd),
+    ((0, 0), (4, 4), stag),
+    ((1, 0), (3, 1), stag),
+    ((0, 1), (1, 3), stag),
+    ((1, 1), (2, 2), stag),
+    ((0, 0), (3, 2), sexes),
+    ((1, 0), (0, 0), sexes),
+    ((0, 1), (0, 0), sexes),
+    ((1, 1), (2, 3), sexes),
+    ((0, 0), (0, 0), chicken),
+    ((1, 0), (1, -1), chicken),
+    ((0, 1), (-1, 1), chicken),
+    ((1, 1), (-2, -2), chicken),
 ]
 
 
-@pytest.mark.parametrize("actions, expected_rewards", testdata)
-def test_batch_outcomes(actions, expected_rewards) -> None:
+@pytest.mark.parametrize("actions, expected_rewards, payoff", testdata)
+def test_batch_outcomes(actions, expected_rewards, payoff) -> None:
     num_envs = 3
     all_ones = jnp.ones((num_envs, 1))
-    payoff = [[2, 2], [3, 0], [0, 3], [1, 1]]
-
     env = SocialDilemmaBaseEnvironment(5, num_envs, payoff)
     env.reset()
 
