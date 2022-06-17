35ac4fe0a6eaab0332c6f2c95f27298b903de73f
Newton Kwan <nk@2398.com>
2022-06-17 04:19:55 +0100

    remove hard coded action space and initialize networks to baseline

--- pax/ppo/networks.py
+++ pax/ppo/networks.py
@@ -13,8 +13,16 @@ class CategoricalValueHead(hk.Module):
         name: Optional[str] = None,
     ):
         super().__init__(name=name)
-        self._logit_layer = hk.Linear(num_values)
-        self._value_layer = hk.Linear(1)
+        self._logit_layer = hk.Linear(
+            num_values,
+            w_init=hk.initializers.Orthogonal(0.01),
+            b_init=hk.initializers.Constant(0),
+        )
+        self._value_layer = hk.Linear(
+            1,
+            w_init=hk.initializers.Orthogonal(1.0),
+            b_init=hk.initializers.Constant(0),
+        )
 
     def __call__(self, inputs: jnp.ndarray):
         logits = self._logit_layer(inputs)
@@ -22,65 +30,24 @@ class CategoricalValueHead(hk.Module):
         return (distrax.Categorical(logits=logits), value)
 
 
-def forward_fn(inputs):
-    layers = []
-    layers.extend(
-        [
-            hk.nets.MLP(
-                [64, 64],
-                w_init=hk.initializers.Orthogonal(jnp.sqrt(2)),
-                b_init=hk.initializers.Constant(0),
-                activate_final=True,
-            ),
-            # TODO: Remove hardcoded action space.
-            CategoricalValueHead(num_values=2),
-        ]
-    )
-    policy_value_network = hk.Sequential(layers)
-    return policy_value_network(inputs)
+def make_network(num_actions: int):
+    """Creates a hk network"""
 
+    def forward_fn(inputs):
+        layers = []
+        layers.extend(
+            [
+                hk.nets.MLP(
+                    [64, 64],
+                    w_init=hk.initializers.Orthogonal(jnp.sqrt(2)),
+                    b_init=hk.initializers.Constant(0),
+                    activate_final=True,
+                ),
+                CategoricalValueHead(num_values=num_actions),
+            ]
+        )
+        policy_value_network = hk.Sequential(layers)
+        return policy_value_network(inputs)
 
-# def critic_network(inputs: jnp.ndarray) -> jnp.ndarray:
-#     flat_inputs = hk.Flatten()(inputs)
-#     mlp = hk.nets.MLP(
-#         [2, 2, 1],
-#         w_init=hk.initializers.Orthogonal(jnp.sqrt(2)),
-#         b_init=hk.initializers.Constant(0),
-#         activation=jax.nn.hard_tanh,
-#         name="critic_network",
-#     )
-#     # TODO: Figure out how to properly change the last layer to init with different weights
-#     # and to preserve the name (currently it's not prepending critic_network/~/)
-#     mlp.layers = mlp.layers[:-1] + (
-#         hk.Linear(
-#             1,
-#             w_init=hk.initializers.Orthogonal(1.0),
-#             b_init=hk.initializers.Constant(0),
-#             name="linear_%d" % (len(mlp.layers) - 1),
-#         ),
-#     )
-#     policy = mlp(flat_inputs)
-#     return policy
-
-
-# def actor_network(inputs: jnp.ndarray) -> jnp.ndarray:
-#     flat_inputs = hk.Flatten()(inputs)
-#     mlp = hk.nets.MLP(
-#         [2, 2, 2],
-#         w_init=hk.initializers.Orthogonal(jnp.sqrt(2)),
-#         b_init=hk.initializers.Constant(0),
-#         activation=jax.nn.hard_tanh,
-#         name="actor_network",
-#     )
-#     # TODO: Figure out how to properly change the last layer to init with different weights
-#     # and to preserve the name (currently it's not prepending actor_network/~/)
-#     mlp.layers = mlp.layers[:-1] + (
-#         hk.Linear(
-#             2,
-#             w_init=hk.initializers.Orthogonal(0.01),
-#             b_init=hk.initializers.Constant(0),
-#             name="linear_%d" % (len(mlp.layers) - 1),
-#         ),
-#     )
-#     actions = mlp(flat_inputs)
-#     return actions
+    network = hk.without_apply_rng(hk.transform(forward_fn))
+    return network
