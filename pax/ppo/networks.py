change step_type logic, fix discounts calculation in gae advantage calculation

pax/ppo/networks.py
@@ -19,14 +19,14 @@ class CategoricalValueHead(hk.Module):
         super().__init__(name=name)
         self._logit_layer = hk.Linear(
             num_values,
-            # w_init=hk.initializers.Orthogonal(0.01), # baseline
-            w_init=hk.initializers.Constant(1),
+            w_init=hk.initializers.Orthogonal(0.01),  # baseline
+            # w_init=hk.initializers.Constant(1),
             with_bias=False,
         )
         self._value_layer = hk.Linear(
             1,
-            # w_init=hk.initializers.Orthogonal(1.0), # baseline
-            w_init=hk.initializers.Constant(1),
+            w_init=hk.initializers.Orthogonal(1.0),  # baseline
+            # w_init=hk.initializers.Constant(1),
             with_bias=False,
         )
 
@@ -43,14 +43,29 @@ def make_network(num_actions: int):
         layers = []
         layers.extend(
             [
-                # hk.nets.MLP(
-                #     # [64, 64],
-                #     [5],
-                #     # w_init=hk.initializers.Orthogonal(jnp.sqrt(2)),
-                #     w_init=hk.initializers.Constant(1),
-                #     with_bias = False,
-                #     activate_final=True,
-                # ),
+                CategoricalValueHead(num_values=num_actions),
+            ]
+        )
+        policy_value_network = hk.Sequential(layers)
+        return policy_value_network(inputs)
+
+    network = hk.without_apply_rng(hk.transform(forward_fn))
+    return network
+
+
+def make_cartpole_network(num_actions: int):
+    """Creates a hk network using the baseline hyperparameters from OpenAI"""
+
+    def forward_fn(inputs):
+        layers = []
+        layers.extend(
+            [
+                hk.nets.MLP(
+                    [64, 64],
+                    w_init=hk.initializers.Orthogonal(jnp.sqrt(2)),
+                    b_init=hk.initializers.Constant(0),
+                    activate_final=True,
+                ),
                 CategoricalValueHead(num_values=num_actions),
             ]
         )
@@ -71,6 +86,31 @@ def make_GRU(num_actions: int):
         """forward function"""
         gru = hk.GRU(hidden_size)
         embedding, state = gru(inputs, state)
+        logits, values = CategoricalValueHead(num_actions)(embedding)
+        return (logits, values), state
+
+    network = hk.without_apply_rng(hk.transform(forward_fn))
+
+    return network, hidden_state
+
+
+def make_GRU_cartpole_network(num_actions: int):
+    hidden_size = 256
+    hidden_state = jnp.zeros((1, hidden_size))
+
+    def forward_fn(
+        inputs: jnp.ndarray, state: jnp.ndarray
+    ) -> Tuple[Tuple[jnp.ndarray, jnp.ndarray], jnp.ndarray]:
+        """forward function"""
+        torso = hk.nets.mlp(
+            [hidden_size],
+            w_init=hk.initializers.Orthogonal(jnp.sqrt(2)),
+            b_init=hk.initializers.Constant(0),
+            activate_final=True,
+        )
+        gru = hk.GRU(hidden_size)
+        embedding = torso(inputs)
+        embedding, state = gru(embedding, state)
         logits, values = CategoricalValueHead(num_actions)(embedding)
         return (logits, values), state
 

pax/ppo/ppo.py
@@ -4,7 +4,7 @@ from typing import Any, Mapping, NamedTuple, Tuple, Dict
 
 from pax import utils
 from pax.ppo.buffer import TrajectoryBuffer
-from pax.ppo.networks import make_network
+from pax.ppo.networks import make_network, make_cartpole_network
 
 from dm_env import TimeStep
 import haiku as hk
@@ -110,7 +110,8 @@ class PPO:
 
             # 'Zero out' the terminated states
             # discounts = gamma * (1 - dones)
-            discounts = gamma * (1 - jnp.zeros_like(dones))
+            discounts = gamma * jnp.where(dones < 2, 1, 0)
+            # discounts = gamma * (1 - jnp.zeros_like(dones))
 
             # this is where the gae function will go.
 
@@ -370,8 +371,8 @@ class PPO:
             dummy_obs = utils.add_batch_dim(dummy_obs)
             initial_params = network.init(subkey, dummy_obs)
             initial_opt_state = optimizer.init(initial_params)
-            for dict_key in initial_params.keys():
-                print(initial_params[dict_key])
+            # for dict_key in initial_params.keys():
+            #     print(initial_params[dict_key])
             return TrainingState(
                 params=initial_params,
                 opt_state=initial_opt_state,
@@ -479,9 +480,13 @@ class PPO:
 def make_agent(args, obs_spec, action_spec, seed: int, player_id: int):
     """Make PPO agent"""
 
-    # Network
-    network = make_network(action_spec)
-    # network = make_network(action_spec.num_values)
+    if args.env_id == "CartPole-v1":
+        print(f"Making network for {args.env_id}")
+        network = make_cartpole_network(action_spec)
+
+    else:
+        print(f"Making network for {args.env_id}")
+        network = make_network(action_spec)
 
     # Optimizer
     batch_size = int(args.num_envs * args.num_steps)

pax/ppo/ppo_gru.py
@@ -4,7 +4,7 @@ from typing import Any, Mapping, NamedTuple, Tuple, Dict
 
 from pax import utils
 from pax.ppo.buffer import TrajectoryBuffer
-from pax.ppo.networks import make_GRU
+from pax.ppo.networks import make_GRU, make_GRU_cartpole_network
 
 from dm_env import TimeStep
 import haiku as hk
@@ -115,9 +115,7 @@ class PPO:
             dones = dones[:-1]
 
             # 'Zero out' the terminated states
-            # This might just be totally wrong?
-            # discounts = gamma * (1 - dones)
-            discounts = gamma * (1 - jnp.zeros_like(dones))
+            discounts = gamma * jnp.where(dones < 2, 1, 0)
 
             delta = rewards + discounts * values[1:] - values[:-1]
             advantage_t = [0.0]
@@ -502,7 +500,14 @@ class PPO:
 def make_gru_agent(args, obs_spec, action_spec, seed: int, player_id: int):
     """Make PPO agent"""
     # Network
-    network, initial_hidden_state = make_GRU(action_spec)
+    if args.env_id == "CartPole-v1":
+        print(f"Making network for {args.env_id}")
+        network, initial_hidden_state = make_GRU_cartpole_network(action_spec)
+
+    else:
+        print(f"Making network for {args.env_id}")
+        network, initial_hidden_state = make_GRU(action_spec)
+
     gru_dim = initial_hidden_state.shape[1]
 
     # Optimizer