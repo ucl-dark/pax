27a7c46c8d2b47e4b0a1c7fe818eeffbba86d83c
Akbir <akbir.94@gmail.com>
2022-08-15 11:09:40 +0200

    ran isort

--- pax/experiment.py
+++ pax/experiment.py
@@ -10,11 +10,11 @@ from pax.env import SequentialMatrixGame
 from pax.hyper.ppo import make_hyper
 from pax.independent_learners import IndependentLearners
 from pax.meta_env import InfiniteMatrixGame, MetaFiniteGame
-from pax.runner import Runner
 from pax.naive.naive import make_naive_pg
 from pax.naive_exact import NaiveExact
 from pax.ppo.ppo import make_agent
 from pax.ppo.ppo_gru import make_gru_agent
+from pax.runner import Runner
 from pax.strategies import (
     Altruistic,
     Defect,
--- pax/independent_learners.py
+++ pax/independent_learners.py
@@ -1,8 +1,8 @@
 from typing import Callable, List
 
+import jax
 import jax.numpy as jnp
 from dm_env import TimeStep
-import jax
 
 
 class IndependentLearners:
