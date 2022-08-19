bf9654daa2eeffb52230e051be494dd34f5b81ae
Newton Kwan <nk@2398.com>
2022-08-19 10:16:55 +0100

    parity test es_params

--- pax/evo_runner.py
+++ pax/evo_runner.py
@@ -180,7 +180,7 @@ class EvoRunner:
         rng, _ = jax.random.split(self.random_key)
 
         # strategy = self.strategy
-        # es_params = self.es_params
+        es_params = self.es_params
         param_reshaper = self.param_reshaper
 
         # param_reshaper = ParameterReshaper(agent1._state.params)
@@ -193,27 +193,27 @@ class EvoRunner:
             )
             # Uncomment for specific param choices
             # Update basic parameters of PGPE strategy
-            es_params = strategy.default_params.replace(
-                sigma_init=self.sigma_init,
-                sigma_decay=self.sigma_decay,
-                sigma_limit=self.sigma_limit,
-                init_min=self.init_min,
-                init_max=self.init_max,
-                clip_min=self.clip_min,
-                clip_max=self.clip_max,
-            )
-            # # Update optimizer-specific parameters of Adam
-            es_params = es_params.replace(
-                opt_params=es_params.opt_params.replace(
-                    lrate_init=self.lrate_init,
-                    lrate_decay=self.lrate_decay,
-                    lrate_limit=self.lrate_limit,
-                    beta_1=self.beta_1,
-                    beta_2=self.beta_2,
-                    eps=self.eps,
-                )
-            )
-            es_params = strategy.default_params
+            # es_params = strategy.default_params.replace(
+            #     sigma_init=self.sigma_init,
+            #     sigma_decay=self.sigma_decay,
+            #     sigma_limit=self.sigma_limit,
+            #     init_min=self.init_min,
+            #     init_max=self.init_max,
+            #     clip_min=self.clip_min,
+            #     clip_max=self.clip_max,
+            # )
+            # # # Update optimizer-specific parameters of Adam
+            # es_params = es_params.replace(
+            #     opt_params=es_params.opt_params.replace(
+            #         lrate_init=self.lrate_init,
+            #         lrate_decay=self.lrate_decay,
+            #         lrate_limit=self.lrate_limit,
+            #         beta_1=self.beta_1,
+            #         beta_2=self.beta_2,
+            #         eps=self.eps,
+            #     )
+            # )
+            # es_params = strategy.default_params
         elif self.algo == "CMA_ES":
             print("Algo: CMA_ES")
             strategy = CMA_ES(
