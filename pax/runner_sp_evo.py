236227a18179b00ef0a4f6ba7e4ef2c761e5d974
Akbir <akbir.94@gmail.com>
2022-09-21 01:11:51 +0100

    add batch memory resets

--- pax/runner_sp_evo.py
+++ pax/runner_sp_evo.py
@@ -421,6 +421,9 @@ class MetaSPEvoRunner:
             rewards_0 = traj_1.rewards.sum(axis=1).mean()
             rewards_1 = traj_2.rewards.sum(axis=1).mean()
 
+            a1_mem = agent1.batch_reset(a1_mem, False)
+            a2_mem = agent2.batch_reset(a2_mem, False)
+
             # run loop against Naive Learner bitches
             a3_state, a3_mem = agent3.batch_init(
                 jax.random.split(rng_nl_1, popsize * num_opps).reshape(
@@ -432,6 +435,7 @@ class MetaSPEvoRunner:
             t_init, env_state = env.runner_reset(
                 (popsize, num_opps, env.num_envs), rng_nl_1
             )
+            # agent 1 vs agent 3
             vals, stack = jax.lax.scan(
                 _outer_nl_rollout,
                 (*t_init, a1_state, a1_mem, a3_state, a3_mem, env_state),
@@ -481,6 +485,8 @@ class MetaSPEvoRunner:
             t_init, env_state = env.runner_reset(
                 (popsize, num_opps, env.num_envs), rng_nl_2
             )
+
+            # agent 2 vs agent 3
             vals, stack = jax.lax.scan(
                 _outer_nl_rollout,
                 (*t_init, a2_state, a2_mem, a3_state, a3_mem, env_state),
