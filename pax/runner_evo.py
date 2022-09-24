ce47f0d4ab31d5ee94ee9cbc4f4e7502df707a66
Newton Kwan <nk@2398.com>
2022-09-24 19:38:22 +0100

    add reset between episodes

--- pax/runner_evo.py
+++ pax/runner_evo.py
@@ -111,6 +111,8 @@ class EvoRunner:
         def _outer_rollout(carry, unused):
             """Runner for trial"""
             t1, t2, a1_state, a1_mem, a2_state, a2_mem, env_state = carry
+            a1_mem = agent1.batch_reset(a1_mem, False)
+            a2_mem = agent2.batch_reset(a2_mem, False)
             # play episode of the game
             vals, trajectories = jax.lax.scan(
                 _inner_rollout,
