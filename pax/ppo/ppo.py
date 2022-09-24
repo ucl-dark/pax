3cf25feae9906d30cee67685649ca14e216d12de
Akbir <akbir.94@gmail.com>
2022-09-24 14:44:02 +0100

    first push

--- pax/ppo/ppo.py
+++ pax/ppo/ppo.py
@@ -265,7 +265,7 @@ class PPO:
                 " num_minibatches={}."
             ).format(batch_size, num_minibatches)
 
-            batch = jax.tree_map(
+            batch = jax.tree_util.tree_map(
                 lambda x: x.reshape((batch_size,) + x.shape[2:]), trajectories
             )
 
@@ -319,10 +319,10 @@ class PPO:
                 key, params, opt_state, timesteps, batch = carry
                 key, subkey = jax.random.split(key)
                 permutation = jax.random.permutation(subkey, batch_size)
-                shuffled_batch = jax.tree_map(
+                shuffled_batch = jax.tree_util.tree_map(
                     lambda x: jnp.take(x, permutation, axis=0), batch
                 )
-                minibatches = jax.tree_map(
+                minibatches = jax.tree_util.tree_map(
                     lambda x: jnp.reshape(
                         x, [num_minibatches, -1] + list(x.shape[1:])
                     ),
@@ -351,7 +351,7 @@ class PPO:
                 length=num_epochs,
             )
 
-            metrics = jax.tree_map(jnp.mean, metrics)
+            metrics = jax.tree_util.tree_map(jnp.mean, metrics)
             metrics["rewards_mean"] = jnp.mean(
                 jnp.abs(jnp.mean(rewards, axis=(0, 1)))
             )
