997544286e2a74dcf38dc126ca534ef5964409de
Newton Kwan <nk@2398.com>
2022-09-26 16:14:28 +0100

    change memory size to 128 to earl_v_tabular

--- pax/ppo/networks.py
+++ pax/ppo/networks.py
@@ -301,7 +301,8 @@ def make_cartpole_network(num_actions: int):
 
 
 def make_GRU_ipd_network(num_actions: int):
-    hidden_size = 25
+    hidden_size = 128
+    # memory_size = args.ppo.memory_size
     hidden_state = jnp.zeros((1, hidden_size))
 
     def forward_fn(
