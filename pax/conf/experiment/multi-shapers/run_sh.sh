e3463aa22313ec13fa1f4cb42ccaae1f35e124cc
alexandrasouly <alexandrasouly@gmail.com>
2023-06-13 17:33:32 +0100

    messed up sh

--- pax/conf/experiment/multi-shapers/run_sh.sh
+++ pax/conf/experiment/multi-shapers/run_sh.sh
@@ -1,7 +1,7 @@
-python3 -m pax.experiment +experiment=multi-shapers/n2pl_2shap_sh
-python3 -m pax.experiment +experiment=multi-shapers/n3pl_3shap_sh
-python3 -m pax.experiment +experiment=multi-shapers/n4pl_2shap_sh
-python3 -m pax.experiment +experiment=multi-shapers/n4pl_3shap_sh
+# python3 -m pax.experiment +experiment=multi-shapers/n2pl_2shap_sh
+# python3 -m pax.experiment +experiment=multi-shapers/n3pl_3shap_sh
+# python3 -m pax.experiment +experiment=multi-shapers/n4pl_2shap_sh
+# python3 -m pax.experiment +experiment=multi-shapers/n4pl_3shap_sh
 python3 -m pax.experiment +experiment=multi-shapers/n5pl_2shap_sh
 python3 -m pax.experiment +experiment=multi-shapers/n5pl_3shap_sh
 python3 -m pax.experiment +experiment=multi-shapers/n5pl_4shap_sh
