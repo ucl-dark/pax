9e3b5648768c38e27aa5619739c98a0b052c00ee
alexandrasouly <alexandrasouly@gmail.com>
2023-06-18 21:23:23 +0100

    run sd

---
+++ pax/conf/experiment/multi-shapers/run_sd.sh
@@ -0,0 +1,3 @@
+python3 -m pax.experiment +experiment=multi-shapers/n5pl_2shap_sd
+python3 -m pax.experiment +experiment=multi-shapers/n5pl_3shap_sd
+python3 -m pax.experiment +experiment=multi-shapers/n5pl_4shap_sd
