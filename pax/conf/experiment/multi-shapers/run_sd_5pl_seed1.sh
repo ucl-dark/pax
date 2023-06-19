66a2b3695cc776c76c09351cc047fae1eabcd089
alexandrasouly <alexandrasouly@gmail.com>
2023-06-19 11:45:04 +0100

    right groups

---
+++ pax/conf/experiment/multi-shapers/run_sd_5pl_seed1.sh
@@ -0,0 +1,3 @@
+python3 -m pax.experiment +experiment=multi-shapers/n5pl_2shap_sd ++seed=1
+python3 -m pax.experiment +experiment=multi-shapers/n5pl_3shap_sd ++seed=1
+python3 -m pax.experiment +experiment=multi-shapers/n5pl_4shap_sd ++seed=1
---
+++ pax/conf/experiment/multi-shapers/run_sd_small.sh
@@ -0,0 +1,5 @@
+python3 -m pax.experiment +experiment=multi-shapers/n2pl_2shap_sd
+python3 -m pax.experiment +experiment=multi-shapers/n3pl_2shap_sd
+python3 -m pax.experiment +experiment=multi-shapers/n3pl_3shap_sd
+python3 -m pax.experiment +experiment=multi-shapers/n4pl_2shap_sd
+python3 -m pax.experiment +experiment=multi-shapers/n4pl_3shap_sd
