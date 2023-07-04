41045f26f28eac38727240f59fd92519de216001
alexandrasouly <alexandrasouly@gmail.com>
2023-07-04 19:00:38 +0100

    script to run multiple seeds

--- pax/conf/experiment/n_player/shaper/run_one_shaper.sh
+++ pax/conf/experiment/n_player/shaper/run_one_shaper.sh
@@ -1,4 +1,4 @@
-# quick ones
+# quick ones to converge
 for SEED in 0 1 2 3 4 
 do
 python3 -m pax.experiment +experiment=n_player/shaper/3pl_sd ++seed=$SEED
@@ -7,22 +7,23 @@ python3 -m pax.experiment +experiment=n_player/shaper/4pl_sh ++seed=$SEED
 python3 -m pax.experiment +experiment=n_player/shaper/4pl_tc ++seed=$SEED
 python3 -m pax.experiment +experiment=n_player/shaper/5pl_sd ++seed=$SEED
 python3 -m pax.experiment +experiment=n_player/shaper/5pl_sh ++seed=$SEED
-
 done
 
-
-# slow ones
+# slow ones but they do converge
 for SEED in 0 1 2 3 4 
 do
 python3 -m pax.experiment +experiment=n_player/shaper/3pl_ipd ++seed=$SEED
 python3 -m pax.experiment +experiment=n_player/shaper/3pl_tc ++seed=$SEED
 python3 -m pax.experiment +experiment=n_player/shaper/5pl_ipd ++seed=$SEED
+done
+
+# I haven't ran this one before - I think it will converge reaosnable but check how fast
+for SEED in 0 1 2 3 4 
+do
 python3 -m pax.experiment +experiment=n_player/shaper/5pl_tc ++seed=$SEED
 done
 
-
-
-# not converging ones
+# not converging ones pls help :(
 for SEED in 0 1 2 3 4 
 do
 python3 -m pax.experiment +experiment=n_player/shaper/3pl_sh ++seed=$SEED
