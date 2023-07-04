9e0c8dd61958345db07ba6819c0996eeddf00264
alexandrasouly <alexandrasouly@gmail.com>
2023-07-04 19:08:05 +0100

    seeds shifted

--- pax/conf/experiment/n_player/shaper/run_one_shaper.sh
+++ pax/conf/experiment/n_player/shaper/run_one_shaper.sh
@@ -1,5 +1,5 @@
 # quick ones to converge
-for SEED in 0 1 2 3 4 
+for SEED in 1 2 3 4 5
 do
 python3 -m pax.experiment +experiment=n_player/shaper/3pl_sd ++seed=$SEED
 python3 -m pax.experiment +experiment=n_player/shaper/4pl_sd ++seed=$SEED
@@ -10,7 +10,7 @@ python3 -m pax.experiment +experiment=n_player/shaper/5pl_sh ++seed=$SEED
 done
 
 # slow ones but they do converge
-for SEED in 0 1 2 3 4 
+for SEED in 1 2 3 4 5
 do
 python3 -m pax.experiment +experiment=n_player/shaper/3pl_ipd ++seed=$SEED
 python3 -m pax.experiment +experiment=n_player/shaper/3pl_tc ++seed=$SEED
@@ -18,13 +18,13 @@ python3 -m pax.experiment +experiment=n_player/shaper/5pl_ipd ++seed=$SEED
 done
 
 # I haven't ran this one before - I think it will converge reaosnable but check how fast
-for SEED in 0 1 2 3 4 
+for SEED in 1 2 3 4 5 
 do
 python3 -m pax.experiment +experiment=n_player/shaper/5pl_tc ++seed=$SEED
 done
 
 # not converging ones pls help :(
-for SEED in 0 1 2 3 4 
+for SEED in 1 2 3 4 5
 do
 python3 -m pax.experiment +experiment=n_player/shaper/3pl_sh ++seed=$SEED
 python3 -m pax.experiment +experiment=n_player/shaper/4pl_ipd ++seed=$SEED
