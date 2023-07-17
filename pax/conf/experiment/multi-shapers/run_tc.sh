79b095072b90076c4f4f86d8727a075986eb3f06
alexandrasouly <alexandrasouly@gmail.com>
2023-07-17 14:14:21 +0100

    3pl op nums

---
+++ pax/conf/experiment/multi-shapers/run_tc.sh
@@ -0,0 +1,6 @@
+python3 -m pax.experiment +experiment=multi-shapers/n3pl_3shap_sh ++num_outer_steps=1000 
+python3 -m pax.experiment +experiment=multi-shapers/n4pl_2shap_sh
+python3 -m pax.experiment +experiment=multi-shapers/n4pl_3shap_sh
+python3 -m pax.experiment +experiment=multi-shapers/n5pl_2shap_sh
+python3 -m pax.experiment +experiment=multi-shapers/n5pl_3shap_sh
+python3 -m pax.experiment +experiment=multi-shapers/n5pl_4shap_sh
