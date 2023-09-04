deb96356de0e3fff7443c7700209c0574bbea4e7
alexandrasouly <alexandrasouly@gmail.com>
2023-09-04 17:03:57 +0100

    5pl script

--- pax/conf/experiment/multi-shapers/timon_5pl.sh
+++ pax/conf/experiment/multi-shapers/timon_5pl.sh
@@ -1,21 +1,6 @@
-for SEED in  1 2 3 4 5
+python3 -m pax.experiment +experiment=multi-shapers/n5pl_4shap_ipd ++num_iters=2000 ++seed=1 ++pop_size=150
+for SEED in  2 3 4 5
 do
-#sh
-python3 -m pax.experiment +experiment=multi-shapers/n5pl_2shap_sh ++num_iters=100 ++seed=$SEED
-python3 -m pax.experiment +experiment=multi-shapers/n5pl_3shap_sh ++num_iters=100 ++seed=$SEED
-python3 -m pax.experiment +experiment=multi-shapers/n5pl_4shap_sh ++num_iters=150 ++seed=$SEED
-
-#sd
-python3 -m pax.experiment +experiment=multi-shapers/n5pl_2shap_sd ++num_iters=150 ++seed=$SEED
-python3 -m pax.experiment +experiment=multi-shapers/n5pl_3shap_sd ++num_iters=150 ++seed=$SEED
-python3 -m pax.experiment +experiment=multi-shapers/n5pl_4shap_sd ++num_iters=150 ++seed=$SEED
-
-#tc
-python3 -m pax.experiment +experiment=multi-shapers/n5pl_2shap_tc ++num_iters=150 ++seed=$SEED
-python3 -m pax.experiment +experiment=multi-shapers/n5pl_3shap_tc ++num_iters=500 ++seed=$SEED
-python3 -m pax.experiment +experiment=multi-shapers/n5pl_4shap_tc ++num_iters=200 ++seed=$SEED
-
-#ipd - pls check max popsize that fits, these take ages to converge
 python3 -m pax.experiment +experiment=multi-shapers/n5pl_2shap_ipd ++num_iters=2500 ++seed=$SEED ++pop_size=150
 python3 -m pax.experiment +experiment=multi-shapers/n5pl_3shap_ipd ++num_iters=2500 ++seed=$SEED ++pop_size=150
 python3 -m pax.experiment +experiment=multi-shapers/n5pl_4shap_ipd ++num_iters=2000 ++seed=$SEED ++pop_size=150
